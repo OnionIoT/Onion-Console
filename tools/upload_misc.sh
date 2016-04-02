038ec173316e0c3467da09365b74850466eaa190
greenbreakfast <lazardemin@gmail.com>
2016-04-02 15:48:26 -0400

    script now prints out the rsync commands being executed

--- tools/upload_misc.sh
+++ tools/upload_misc.sh
@@ -22,10 +22,28 @@ echo "Omega IP: $ipAddr"
 
 
 # transfer files
-rsync --progress -rv ./acl.d/* root@$ipAddr:/usr/share/rpcd/acl.d
-rsync --progress -rv ./rpcd/onion-console.sh root@$ipAddr:/usr/libexec/rpcd/onion-console
-rsync --progress -rv ./bin/factory-reset.sh root@$ipAddr:/usr/sbin/factory-reset
-rsync --progress -rv ./bin/power-down.sh root@$ipAddr:/usr/sbin/power-down
-rsync --progress -rv ./bin/change-password.sh root@$ipAddr:/usr/sbin/change-password
-rsync --progress -rv ./bin/disk-space.sh root@$ipAddr:/usr/sbin/disk-space
+cmd="rsync --progress -rv ./acl.d/* root@$ipAddr:/usr/share/rpcd/acl.d"
+echo "$cmd"
+$cmd
+
+cmd="rsync --progress -rv ./rpcd/onion-console.sh root@$ipAddr:/usr/libexec/rpcd/onion-console"
+echo "$cmd"
+$cmd
+
+cmd="rsync --progress -rv ./bin/factory-reset.sh root@$ipAddr:/usr/sbin/factory-reset"
+echo "$cmd"
+$cmd
+
+cmd="rsync --progress -rv ./bin/power-down.sh root@$ipAddr:/usr/sbin/power-down"
+echo "$cmd"
+$cmd
+
+cmd="rsync --progress -rv ./bin/change-password.sh root@$ipAddr:/usr/sbin/change-password"
+echo "$cmd"
+$cmd
+
+cmd="rsync --progress -rv ./bin/disk-space.sh root@$ipAddr:/usr/sbin/disk-space"
+echo "$cmd"
+$cmd
+
 
